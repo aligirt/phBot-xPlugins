44f9f8f674187e1042f86779f20ef0b3c9c2d8e5
Ali Tayyib GİRİT <133659643+aligirt@users.noreply.github.com>
2023-12-27 00:17:45 +0300

    Update xAutoDungeon.py

--- xAutoDungeon.py
+++ xAutoDungeon.py
@@ -14,7 +14,7 @@ pUrl = 'https://raw.githubusercontent.com/JellyBitz/phBot-xPlugins/master/xAutoD
 # ______________________________ Initializing ______________________________ #
 
 DIMENSIONAL_COOLDOWN_DELAY = 7200 # seconds (2 hours)
-WAIT_DROPS_DELAY_MAX = 10 # seconds
+WAIT_DROPS_DELAY_MAX = 30 # seconds
 COUNT_MOBS_DELAY = 1.0 # seconds
 
 # Globals
